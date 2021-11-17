0975e6fd80627aed8b4ae51b6ee22abe4b67e298
JMuckian94 <johnmuckian@gmail.com>
2021-11-17 10:32:01 +0000

    Changed base to lastlifemain all instances

--- lastlifemain/urls.py
+++ lastlifemain/urls.py
@@ -1,4 +1,4 @@
-"""base URL Configuration
+"""lastlifemain URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -6,7 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-lastlifemaind views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
