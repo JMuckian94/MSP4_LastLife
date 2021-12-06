5ca8738c11404a2c1f8f6dd63271e88bf5cbed3a
JMuckian94 <johnmuckian@gmail.com>
2021-12-06 15:20:01 +0000

    Added home app and templates

--- lastlifemain/settings.py
+++ lastlifemain/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'lastlifemain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(lastlifemain_DIR, 'templates'),
+            os.path.join(lastlifemain_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
