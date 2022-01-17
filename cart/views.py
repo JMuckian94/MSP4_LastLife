ce27ef8cbb3a7dddbe0d1b487ef471beed83dc2e
JMuckian94 <johnmuckian@gmail.com>
2022-01-17 12:59:24 +0000

    Added quantity selectors and scripts WIP

--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 def view_cart(request):
     """ A view that renders the shopping cart contents page """
 
-    return render(request, 'cart/cart.html')
+    return render(request, 'cart.html')
 
 
 def add_to_cart(request, item_id):
