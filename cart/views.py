f2e6ebdbccf85e0573999f33dd2a4d7cef5a9ef4
JMuckian94 <johnmuckian@gmail.com>
2022-01-19 16:43:12 +0000

    Added template logic to include toast messages, updated cart views

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_cart(request):
@@ -10,14 +15,22 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a certain quantity of the specific product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request,
+                        (f'Updated {product.name} '
+                         f'quantity to {cart[item_id]}'))
+
     else:
         cart[item_id] = quantity
+        messages.success(request,
+                        (f'Updated {product.name}'
+                         f'quantity to {cart[item_id]}'))
 
     request.session['cart'] = cart
     return redirect(redirect_url)
