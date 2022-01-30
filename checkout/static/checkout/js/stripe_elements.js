d9295b9a05fb122643a0bc138794de2c90bf76dd
JMuckian94 <johnmuckian@gmail.com>
2022-01-30 16:43:23 +0000

    Added loading overlay spinner and stripe pop up

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -49,8 +49,10 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true });
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -59,13 +61,15 @@ form.addEventListener('submit', function(ev) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
-            <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>`;
-        $(errorDiv).html(html);
-        card.update({ 'disabled': false });
-        $('#submit-button').attr('disabled', false);
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
