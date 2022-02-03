26ec7755e1eabee1925cab13ba3dcc9e308c0077
JMuckian94 <johnmuckian@gmail.com>
2022-02-03 23:29:09 +0000

    Added favicon

--- lastlifemain/settings.py
+++ lastlifemain/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [lastlifemain.herokuapp.com, 'localhost']
+ALLOWED_HOSTS = ['lastlifemain.herokuapp.com', 'localhost']
 
 
 # Application definition
